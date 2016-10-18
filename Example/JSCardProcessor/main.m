60313cbf60261ab3684fbaeb54ffdca766542c3c
John Setting <jsetting32@gmail.com>
2016-10-18 11:20:20 -0700

    Initial commit

---
+++ Example/JSCardProcessor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JSCardProcessor
+//
+//  Created by John Setting on 10/18/2016.
+//  Copyright (c) 2016 John Setting. All rights reserved.
+//
+
+@import UIKit;
+#import "JSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
